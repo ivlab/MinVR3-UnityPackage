fd22b24c1cbe6a453f669c0e22e4ab26f4a46e10
Julia Pan <pan00079@umn.edu>
2022-09-14 16:55:52 -0500

    working prototype v1

--- Runtime/Scripts/Tests/sphereMove.cs
+++ Runtime/Scripts/Tests/sphereMove.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using IVLab.MinVR3;
 
+/// <summary>
+/// This script  
+/// is under active development and
+/// calculates the position of two spheres and the angle between them
+/// using IR lights and a webcam for tracking coordinates in the dome 
+/// 
+/// May need to configure public variables for this script in the inspector
+/// Events: (Touch 0 and 1: up, position, and down for each)
+/// 
+/// InputDevices needs touchTUIO component added
+/// </summary>
+
+
 namespace IVLab.MinVR3.TUIO11 {
     public class sphereMove : MonoBehaviour, IVREventListener {
-        //get sphere object
-        //get coordinate
-        //move sphere object based on coordinate -- this will require the dome conversion
                
         public int distanceMultiplier;
         float m_Angle;
@@ -26,7 +36,8 @@ namespace IVLab.MinVR3.TUIO11 {
         public GameObject movingSphereA;
         public GameObject movingSphereB;
 
-        public void OnVREvent(VREvent vrEvent) {
+        public void OnVREvent(VREvent vrEvent)
+        {
             if (vrEvent.Matches(touchDownEventA)) {
                 movingSphereA.SetActive(true);
                 Vector2 xyA = vrEvent.GetData<Vector2>();
@@ -37,25 +48,18 @@ namespace IVLab.MinVR3.TUIO11 {
             } else if (vrEvent.Matches(touchUpEventA)) {
                 movingSphereA.SetActive(false);
             }
-
             
-            else if (vrEvent.Matches(touchDownEventB))
-            {
+            else if (vrEvent.Matches(touchDownEventB)) {
                 movingSphereB.SetActive(true);
                 Vector2 xyB = vrEvent.GetData<Vector2>();
                 movingSphereB.transform.position = new Vector3(xyB.x * distanceMultiplier, xyB.y * distanceMultiplier, 0);
-            }
-            else if (vrEvent.Matches(touchMoveEventB))
-            {
+            } else if (vrEvent.Matches(touchMoveEventB)) {
                 Vector2 xyB = vrEvent.GetData<Vector2>();
-                movingSphereB.transform.position = new Vector3(xyB.x * distanceMultiplier, xyB.y * distanceMultiplier, 0);
-            }
-            else if (vrEvent.Matches(touchUpEventB))
-            {
+                movingSphereB.transform.position = new Vector3(xyB.x * distanceMultiplier, xyB.y * distanceMultiplier, 0); 
+            } else if (vrEvent.Matches(touchUpEventB)) {
                 movingSphereB.SetActive(false);
             }
         }
-
         
         void OnEnable() {
             StartListening();
@@ -77,11 +81,11 @@ namespace IVLab.MinVR3.TUIO11 {
         {
             if ((movingSphereA.activeSelf == true) && (movingSphereB.activeSelf == true))
             {
-                //fetch positions of moving sphere A and B
+                //fetch positions of movingSphere A and B
                 sphereAxyz = new Vector3(movingSphereA.transform.position.x, movingSphereA.transform.position.y, movingSphereA.transform.position.z);
                 sphereBxyz = new Vector3(movingSphereB.transform.position.x, movingSphereB.transform.position.y, movingSphereA.transform.position.z);
-                //m_Angle = Vector2.Angle(sphereAxy, sphereBxy);
 
+                //calculate 2D angle between both objects
                 x = movingSphereB.transform.position.x - movingSphereA.transform.position.x;
                 y = movingSphereB.transform.position.y - movingSphereA.transform.position.y;
                 m_Angle = Mathf.Atan2(x, y) * Mathf.Rad2Deg;
@@ -110,14 +114,13 @@ namespace IVLab.MinVR3.TUIO11 {
             movingSphereA.SetActive(false);
         }
 
+
         // Update is called once per frame
         void Update() {
             //TUIO.TuioCursor
             //touchTuio.addTuioCursor
 
             CalculateAngleBetweenPoints();
-
-            
         }
     }
 }
